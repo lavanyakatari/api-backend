b451e7c40c4c4f4b075dbd7c129d2a3952b37aa9
lavanyakatari <katarilavanya881@gmail.com>
2025-06-24 15:14:41 +0530

    added the delete and update routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,54 @@
 import User from "../models/User.js";
 
-// GET /api/users
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
 
-export const getUsers = async (req,res) => {
-    try {
-        const users = await User.find();
-        res.send(users);
-    } catch (err) {
-        res.status(500).json({error : 'Server error'})
+export const createUser = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const newUser = new User({ name, email });
+    await newUser.save();
+    res.status(201).json({ message: 'User created', user: newUser });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
     }
-}
+    res.status(200).json({ message: 'User deleted' });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
-// POST /api/users
-
-export const createUser = async (req,res) => {
-    try {
-        const {name,email} = req.body;
-        const newUser = new User({name,email});
-        await newUser.save();
-        res.status(201).json({message:'user created' , user:newUser})
-    } catch (err) {
-        res.status(500).json({message:'internal server'})
+export const updatedUsers = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { name, email },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
     }
+    res.status(200).json({ message: 'User updated', user: updatedUser });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
-}
 
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getUsers, createUser } from '../controllers/userController.js';
+import { getUsers, createUser, updatedUsers, deleteUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/users', getUsers);     
-router.post('/users', createUser);  
+router.post('/users', createUser); 
+router.put('/user/:id', updatedUsers);  
+router.delete('/user/:id', deleteUser); 
 
 export default router;
 
